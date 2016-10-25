5fd465a3003ab66b1478dc27637a4015e6c3af6b
michal dub <michal.dub@fragaria.cz>
2016-10-25 13:42:31 +0200

    changed devtool for devel, sync with angular2 doc

--- config/webpack.develop.js
+++ config/webpack.develop.js
@@ -44,7 +44,7 @@ module.exports = webpackMerge.smart(commonConfig, {
    * See: http://webpack.github.io/docs/configuration.html#devtool
    * See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
    */
-  devtool: 'cheap-module-source-map',
+  devtool: 'cheap-module-eval-source-map',
 
   /**
    * Options affecting the output of the compilation.
