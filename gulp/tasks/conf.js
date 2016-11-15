68e022ccc568e7dd6cd6fb5a8d219943f7c4ec52
Ondřej Chmelař <ondrej.chmelar@fragaria.cz>
2016-11-15 14:32:09 +0100

    rename config files, add prod config

--- gulp/tasks/conf.js
+++ gulp/tasks/conf.js
@@ -5,12 +5,11 @@ var path = require('path');
 var prompt = require('gulp-prompt');
 
 var locations = {
-  inputFile: 'src/assets/configs/devel.js',
-  outputFile: 'src/assets/configs/env.js',
+  inputFile: 'src/assets/configs/dev.js',
+  outputFile: 'src/assets/configs/config.js',
 };
 
 function userChoice(userInputs) {
-  console.log(userInputs.ans, userInputs.ans in {'n': 0, 'no': 0});
   if (userInputs.ans && !(userInputs.ans in {'n': 0, 'no': 0}) ) {
     fs.copySync(locations.inputFile, locations.outputFile);
     console.log('Config "' + locations.outputFile + '" was replaced by "' + locations.inputFile + '"');
@@ -29,10 +28,10 @@ gulp.task('init-conf', function() {
       gulp.src(locations.inputFile).pipe(prompt.prompt([{
         type: 'input',
         name: 'ans',
-        message: 'File "' + locations.outputFile + '" was not created because of already exists. Do you want replace it?'
+        message: 'File "' + locations.outputFile + '" was not created because it already exists. Do you want replace it?'
       }], userChoice));
     }
   } else {
-    console.error('Can create file "' + locations.outputFile + '" because of source file "' + locations.inputFile + '" does not exists');
+    console.error('Can\'t create file "' + locations.outputFile + '" because of source file "' + locations.inputFile + '" does not exist.');
   }
 });
---
+++ src/assets/configs/prod.js
@@ -0,0 +1,7 @@
+var appConfig = {
+  "loggingLevel": 2,
+  "loggingAllowConsoleCatch": true,
+  "itemsApiUrl": "https://private-0f9a88-itemsapi2.apiary-mock.com/items"
+};
+
+window["app-config"] = window["app-config"] || appConfig;
