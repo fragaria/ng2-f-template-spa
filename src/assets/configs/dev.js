56a7d7e128ad38529c7d393e0448f492b977e12c
michal dub <michal.dub@fragaria.cz>
2016-11-29 11:40:43 +0100

    used path without / in language prefix

--- src/assets/configs/dev.js
+++ src/assets/configs/dev.js
@@ -4,7 +4,7 @@ var appConfig = {
   "logging.allowConsoleCatch": true,
   "item.apiUrl": "api/items",
   "language.resources": "i18n/resources?lang=${lang}&module=${module}",
-  "language.prefix": '/assets/i18n',
+  "language.prefix": 'assets/i18n',
   "language.suffix": '.json',
   "language.datePattern": "fullDate",
   "language.numberDigits": "1.0-3"
